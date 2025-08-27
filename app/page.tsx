0af290468124e7ae90d91ca016f0aaf5ffa13b0a
Evandro Monteiro Lima <goodvandro@gmail.com>
2025-08-27 09:10:55 +0000

    Reset develop-0

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { auth } from "@clerk/nextjs/server"
-import HomePage from "./home-page"
-
-export default async function Page() {
-  const { userId } = auth()
-
-  return <HomePage userId={userId} />
-}
